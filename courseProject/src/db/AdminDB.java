9e58c664cd2d2f65045923a49db446dae32b91f4
dorianbenitez <48695978+dorianbenitez@users.noreply.github.com>
2019-03-30 05:18:53 -0500

    Create AdminDB.java

---
+++ courseProject/src/db/AdminDB.java
@@ -0,0 +1,17 @@
+package db;
+/**
+ * 
+ * @author mehra
+ * MYSQL database schema :coursedatabase
+ * user :root
+ * pass :Chump4870
+ * 
+ */
+public interface AdminDB {
+
+	String USER="root";
+	String PASS="Chump4870";
+	String CONN_URL="jdbc:mysql://127.0.0.1:3306/courseDatabase";
+
+	
+}
