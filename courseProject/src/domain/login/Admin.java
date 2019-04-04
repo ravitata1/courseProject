1c965cee9059ffe92712e56025bac0c2b2d63222
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 12:51:03 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/domain/login/Admin.java
+++ courseProject/src/domain/login/Admin.java
@@ -1,26 +1,38 @@
+//needed package
 package domain.login;
 
 public class Admin {
 
 
+		//instance variables
 		private String username;
 		private String password;
 		private String name;
 		public String getUsername() {
 			return username;
 		}
+	
+		//modifier method for username
 		public void setUsername(String username) {
 			this.username = username;
 		}
+		
+		//accessor method for password
 		public String getPassword() {
 			return password;
 		}
+	
+		//modifier method for password
 		public void setPassword(String password) {
 			this.password = password;
 		}
+	
+		//accessor method for name
 		public String getName() {
 			return name;
 		}
+	
+		//modifier method for name
 		public void setName(String name) {
 			this.name = name;
 		}
