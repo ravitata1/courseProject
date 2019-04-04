b9f0a1cabc37ab7b0259cfc5beeb25aac4192c1a
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 12:44:22 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/db/AdminDB.java
+++ courseProject/src/db/AdminDB.java
@@ -7,8 +7,11 @@ package db;
  * pass :Chump4870
  * 
  */
-public interface AdminDB {
 
+//interface to contain Admin login details
+public interface AdminDB {
+	
+	//login credentials for admin login
 	String USER="root";
 	String PASS="Chump4870";
 	String CONN_URL="jdbc:mysql://127.0.0.1:3306/courseDatabase";
