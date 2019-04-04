79463c8cec040d1087e1f1ad41c4d6c520d812ee
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 12:48:24 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/db/MyDB.java
+++ courseProject/src/db/MyDB.java
@@ -7,8 +7,11 @@ package db;
  * pass :Chump4870
  * 
  */
+
+//defines database interface
 public interface MyDB {
 
+	//login credentials
 	String USER="root";
 	String PASS="Chump4870";
 	String CONN_URL="jdbc:mysql://127.0.0.1:3306/courseDatabase";
