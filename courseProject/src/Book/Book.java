f362bef9f0ae977ee7ac084d4e10faae2295cddb
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 12:25:56 -0500

    Commented Code
    Ravi just commented the code.

--- courseProject/src/Book/Book.java
+++ courseProject/src/Book/Book.java
@@ -1,12 +1,15 @@
 package Book;
 
 public class Book {
+	
+	//instance variables
 	private String title;
 	private String ISBN;
 	private double price;
 	private String author;
 	private String link;
 	
+	//default constructor
 	public Book(String t, String a, String i, String l){
 		title = t;
 		author = a;
@@ -14,42 +17,62 @@ public class Book {
 		// price = p;
 		link = l;
 	}
+	
+	//modifier method for title
 	public void setTitle(String t)
 	{
 		title = t;
 	}
+	
+	//accessor method for title
 	public String getTitle()
 	{
 		return title;
 	}
+	
+	//modifier method for ISBN
 	public void setISBN(String i)
 	{
 		ISBN = i;
 	}
+	
+	//accessor method for ISBN
 	public String getISBN()
 	{
 		return ISBN;
 	}
+	
+	//modifier method for author
 	public void setAuthor(String a)
 	{
 		author = a;
 	}
+	
+	//accessor method for author
 	public String getAuthor()
 	{
 		return author;
 	}
+	
+	//modifier method for price
 	public void setPrice(double d)
 	{
 		price = d;
 	}
+	
+	//accessor method for price
 	public double getPrice()
 	{
 		return price;
 	}
+	
+	//modifier method for link
 	public void setLink(String l)
 	{
 		link = l;
 	}
+	
+	//accessor method for link
 	public String getLink()
 	{
 		return link;
