dc50aa0233d180e1bbee022c0d0b7116fc053578
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 23:41:24 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/domain/login/Login.java
+++ courseProject/src/domain/login/Login.java
@@ -7,22 +7,33 @@ package domain.login;
  */
 
 public class Login {
+	
+	//instance variables
 	private String username;
 	private String password;
 	
+	//constructor
 	public Login(String username, String pass){
 		this.username = username;
 		this.password = pass;
 	}
+	
+	//accessor method for username
 	public String getUsername() {
 		return username;
 	}
+	
+	//modifier method for username
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	//accessor method for password
 	public String getPassword() {
 		return password;
 	}
+	
+	//modifier method for password
 	public void setPassword(String password) {
 		this.password = password;
 	}
