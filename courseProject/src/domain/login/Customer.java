cdb16d0be19058a4f8a9cd633e87ff7c943ea4f3
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 23:34:36 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/domain/login/Customer.java
+++ courseProject/src/domain/login/Customer.java
@@ -7,24 +7,30 @@ package domain.login;
 
 public class Customer {
 
+	//instance variables
 	private String username;
 	private String password;
 	private String name;
 	public String getUsername() {
 		return username;
 	}
+	//modifier method for username
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	//accesser method for password
 	public String getPassword() {
 		return password;
 	}
+	//modifier method for password
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	//accessor method for name
 	public String getName() {
 		return name;
 	}
+	//modifier method for name
 	public void setName(String name) {
 		this.name = name;
 	}
