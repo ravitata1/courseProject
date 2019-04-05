6c94617fd5d891f538d2750be4408926799c7ac7
ravitata1 <49286202+ravitata1@users.noreply.github.com>
2019-04-04 23:44:37 -0500

    Commented Code
    Code has been commented by Ravi.

--- courseProject/src/domain/login/LoginController.java
+++ courseProject/src/domain/login/LoginController.java
@@ -1,3 +1,4 @@
+//package and import statements
 package domain.login;
 
 import java.io.IOException;
@@ -19,32 +20,37 @@ public class LoginController extends HttpServlet {
     
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		//new customer object
 		CustomerDao customerDao = new CustomerDaoImpl();
 		
-		
+		//retrieves login credentials
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String submitType = request.getParameter("submit");
 		
+		//new login object
 		Login login = new Login(username, password);
 		
 		
-		
+		//creates new customer object
 		Customer c = customerDao.validateCustomer(login);
 
-		
+		//if user is logging in
 		if(submitType.equals("login") && c!=null && c.getName()!=null){
 			request.setAttribute("message", "Hello "+c.getName() + ",");
 			request.getRequestDispatcher("welcome.jsp").forward(request, response);
-		}else if(submitType.equals("register")){
+		}
+		//if user is registering an account
+		else if(submitType.equals("register")){
 			c.setName(request.getParameter("name"));
 			c.setUsername(request.getParameter("username"));
 			c.setPassword(request.getParameter("password"));
 			customerDao.register(c);
 			request.setAttribute("successMessage", "Registration done, please login!");
 			request.getRequestDispatcher("login.jsp").forward(request, response);
-		}		
+		}
+		
+		//print error message
 		else{
 			request.setAttribute("message", "Data Not Found! Please register!");
 			request.getRequestDispatcher("register.jsp").forward(request, response);
